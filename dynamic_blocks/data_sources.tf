ad64d70603f69ccc1d53068f3edd833bb0d18e51
Michael <michael.ugbechie@capgemini.com>
2021-01-18 16:33:22 +0000

    dynamic variab;es

---
+++ dynamic_blocks/data_sources.tf
@@ -0,0 +1,21 @@
+data "aws_availability_zones" "azs" {
+  state = "available"
+
+  filter {
+    name   = "zone-name"
+    values = ["us-east-2*"]
+  }
+}
+
+
+
+data "external" "echo" {
+
+   program = [ "bash", "-c" , "cat /dev/stdin"]
+
+   query = {
+     foo = "bar"
+   }
+
+}
+
---
+++ dynamic_blocks/outputs.tf
@@ -0,0 +1,16 @@
+output "alb_dns_name" {
+  value       = aws_lb.nlb.dns_name
+  description = "The domain name of the loadbalancer"
+}
+
+
+output "echo" {
+
+   value  = data.external.echo.result
+}
+
+output "echo_foo" {
+
+    value = data.external.echo.result.foo
+
+}
